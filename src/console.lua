7179754d7dafa0f0963b80851f2c89544c0e1f0c
JakeOJeff <realjakeojeff@gmail.com>
2025-09-19 00:33:05 +0530

    Added Backspace and working input entering

--- src/console.lua
+++ src/console.lua
@@ -1,32 +1,34 @@
 local console = {}
 
-
 function console:load()
     self.currentLine = 1
-    self.lines = {
-        content = {
-            
-        }
-    }
+    self.lines = {{
+        content = {}
+    }}
 
 end
 
-
 function console:update(dt)
-
-
 
 end
 
 function console:textinput(t)
-    
-    table.insert(self.content, t)
+
+    table.insert(self.lines[self.currentLine].content, t)
 
 end
 
+function console:keypressed(key)
+    if key == "backspace" then
+        self.lines[self.currentLine].content = string.sub(self.lines[self.currentLine].content, 1, -2)
+    end
+end
 
 function console:draw()
-
+    lg.setFont(fontBaS)
+    for i = 1, #self.lines do
+        love.graphics.print("1. " .. table.concat(self.lines[i].content))
+    end
 
 end
 

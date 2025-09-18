99c999274c693c35bf20ad4790e6ca3b2d34036b
JakeOJeff <realjakeojeff@gmail.com>
2025-09-18 22:23:00 +0530

    Added hardcoded kernel checking for temporary

--- src/start.lua
+++ src/start.lua
@@ -22,8 +22,35 @@ function start:load()
 
     self.textWall = {}
     self.textWall.text = {
-        "text"
+       "Initializing kernel...",
+        "Loading graphics driver v%s...",
+        "Mounting filesystem [%s]...",
+        "Checking memory: %d MB OK",
+        "Connecting to system bus...",
+        "Driver [%s] initialized",
+        "Module [%s]... SUCCESS",
+        "Scanning PCI devices... %d found",
+        "Starting background daemon [%s]",
+        "Authentication services... READY",
+        "System entropy pool seeded",
+        "Launching user interface..."
     }
+
+    self.drivers = { "TWEEN", "TIMER", "SCENERY", "PHYSICS", "AUDIO", "NET", "INPUT" }
+    self.fs = { "ext4", "fat32", "ntfs", "customfs" }
+    self.daemons = { "watchdog", "sync", "power", "gfxd", "audiod", "netd" }
+    
+    math.randomseed(os.time())
+    for i = 1, 25 do
+        local msg = self.textWall.text[math.random(#self.textWall.text)]
+        msg = msg:gsub("%%s", function()
+            local pick = ({self.drivers, self.fs, self.daemons})[math.random(3)]
+            return pick[math.random(#pick)]
+        end)
+        msg = msg:gsub("%%d", tostring(math.random(0, collectgarbage("count"))))
+        table.insert(self.textWall.text, msg)
+    end
+    table.insert(self.textWall.text, "=== SYSTEM LOADED SUCCESSFULLY ===")
 
     -- Credits scrolling setup
     self.textWall.scrollY = wH + 50 * scale -- start below screen
@@ -64,15 +91,18 @@ end
 function start:draw()
     -- Draw scrolling text credits BEHIND gameover screen
     lg.setScissor(0, self.screen.y + self.screen.height + 50 * scale, wW, wH - self.screen.y * scale)
+    lg.setFont(fontBaM)
     if self.textWall.started then
         for i, line in ipairs(self.textWall.text) do
-            local textY = self.textWall.scrollY + (i * 40)
+            local textY = self.textWall.scrollY + (i * fontBaM:getHeight() + 10)
             lg.printf(line, 0, textY, wW, "center")
         end
     end
+    lg.setColor(0, 1, 0, 0.5)
+    lg.rectangle("fill", 0, wH/2, wW, 40)
+    lg.setColor(1,1,1)
     lg.setScissor()
 
-    -- Draw gameover screen
     lg.draw(
         self.screen.img,
         self.screen.x,

aa0b035b3a75dbfafd468be488108a1b078f8400
JakeOJeff <realjakeojeff@gmail.com>
2025-09-19 02:50:11 +0530

    successfully made a command and execution system

--- src/console.lua
+++ src/console.lua
@@ -1,4 +1,5 @@
 local console = {}
+local commands = require "src.implementations.commands"
 
 function console:load()
     self.currentLine = 1
@@ -21,15 +22,38 @@ end
 function console:keypressed(key)
     if key == "backspace" then
         self.lines[self.currentLine].content = string.sub(self.lines[self.currentLine].content, 1, -2)
+    elseif key == "return" then
+        self:execute()
     end
 end
 
 function console:draw()
     lg.setFont(fontBaS)
     for i = 1, #self.lines do
-        love.graphics.print("1. " .. table.concat(self.lines[i].content))
+        love.graphics.print(i .. ". " .. table.concat(self.lines[i].content), 10, 10 + (i * fontBaS:getHeight() + 5))
     end
 
+end
+
+function console:execute()
+    local words = self:splitContent(table.concat(self.lines[self.currentLine].content))
+    local buildCommand = commands
+    local index = 1
+    while #words ~= 0 do
+        buildCommand = buildCommand[words[index]]
+        table.remove(words, 1)
+    end
+    if buildCommand then
+        print(buildCommand())
+    end
+end
+
+function console:splitContent(str)
+    local splitWords = {}
+    for word in string.gmatch(str, "%S+") do
+        table.insert(splitWords, word)
+    end
+    return splitWords
 end
 
 return console
---
+++ src/implementations/commands.lua
@@ -0,0 +1,13 @@
+local commands = {
+
+    
+}
+commands["get"] = {
+    ["version"] = function()
+        return "1.0"
+    end 
+}
+
+
+
+return commands
